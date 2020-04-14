0069d0ea71777e510f52eb0506c75379ece36c3c
Maczi01 <mateusz.w.twardy@gmail.com>
2020-04-15 00:24:25 +0200

    add github projects

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,24 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
-
-
 console.log(`Hello world!`);
+const list = document.querySelector(".projects__list");
+fetch("https://api.github.com/users/maczi01/repos?sort=updated&direction=desc")
+    .then(res => res.json()).then(res => {
+    const repos = res;
+    repos.map(r => {
+        const {name, html_url, description, homepage} = r
+        list.innerHTML += `
+  <li class="project">
+        <div class="project__container">
 
-
+        <img src="../assets/img/githubico.svg" class="project__logo" alt="logo github">
+        <h3 class="project__title">${name}</h3>
+        <p class="project__description">${description}</p>
+    </div>
+    <footer class="project__footer">
+        <a class="project__link project__link--demo" href="${html_url}">Demo</a>
+        <a class="project__link project__link--code" href="${homepage}">Repository</a>
+        </footer>
+        </li>`
+    })
+})
